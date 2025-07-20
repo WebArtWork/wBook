6ad18ba328796b8e217215f315805bbe1891be49
Honchar Denys <crackeraki@gmail.com>
2025-07-20 12:10:13 +0300

    upgrade code

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	preset: 'jest-preset-angular',
-	setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-	testMatch: ['**/+(*.)+(spec).+(ts)?(x)'], // Matches .spec.ts files
-	collectCoverage: true,
-	coverageDirectory: 'coverage',
-	moduleFileExtensions: ['ts', 'js', 'html'],
-	collectCoverage: false
-};
